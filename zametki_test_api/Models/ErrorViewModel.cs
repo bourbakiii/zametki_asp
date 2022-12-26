94490eb379cff1079170bede8a5d6cc70cfe3418
bourbakiii <gabisov03912@gmail.com>
2022-12-26 22:36:15 +0300

    Base changes

---
+++ zametki_test_api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace zametki_test_api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
